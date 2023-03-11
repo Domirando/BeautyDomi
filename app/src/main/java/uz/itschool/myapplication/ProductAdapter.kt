8a722cd5a993c45cbd7ce940f575ae82a496f8df
domirando <maisiedev@gmail.com>
2023-03-11 14:39:23 +0500

    added product activity and pincode functionality

--- app/src/main/java/uz/itschool/myapplication/ProductAdapter.kt
+++ app/src/main/java/uz/itschool/myapplication/ProductAdapter.kt
@@ -1,9 +1,7 @@
 package uz.itschool.myapplication
 
 import android.graphics.Color
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
